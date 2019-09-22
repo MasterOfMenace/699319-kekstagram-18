958d23c083e89db7f897a240bc4d11d455554f7a
MasterOfMenace <nero_wolf@mail.ru>
2019-09-22 19:16:36 +0300

    Написал код просмотра первой фотографии массива фотографий в полном размере

--- js/main.js
+++ js/main.js
@@ -80,3 +80,57 @@ var template = document.querySelector('#picture').content.querySelector('.pictur
 var photosContainer = document.querySelector('.pictures');
 
 renderPhotos(photos);
+
+//отсюда код доп. задания, чтобы не запутатться оставил пока так
+
+function showElement(element) {
+  document.querySelector(element).classList.remove('hidden');
+}
+
+function visuallyHideElement(element) {
+  document.querySelector(element).classList.add('visually-hidden');
+}
+
+function renderBigPhoto(photo) {
+  bigImg.src = photo.url;
+  likesCount.textContent = photo.likes;
+  commentsCount.textContent = photo.comments.length;
+  photoDescription.textContent = photo.description;
+}
+
+function createCommentsElement(photo) {
+  var fragment = document.createDocumentFragment();
+
+  for (var i = 0; i < photo.comments.length; i++) {
+    var commentsElement = document.querySelector('.social__comment').cloneNode(true);
+    var commentAvatar = commentsElement.querySelector('.social__picture');
+    var commentText = commentsElement.querySelector('.social__text');
+    commentAvatar.src = photo.comments[i].avatar;
+    commentAvatar.alt = photo.comments[i].name;
+    commentText.textContent = photo.comments[i].message;
+    fragment.appendChild(commentsElement);
+  }
+  return fragment;
+}
+
+function renderComments(photo) {
+  var fragment = createCommentsElement(photo);
+  commentsList.innerHTML = '';
+  commentsList.appendChild(fragment);
+}
+
+showElement('.big-picture');
+
+var bigImg = document.querySelector('.big-picture__img').getElementsByTagName('img')[0];
+var likesCount = document.querySelector('.likes-count');
+var commentsCount = document.querySelector('.comments-count');
+var photoDescription = document.querySelector('.social__caption');
+
+renderBigPhoto(photos[0]);
+
+var commentsList = document.querySelector('.social__comments');
+
+renderComments(photos[0]);
+
+visuallyHideElement('.social__comment-count');
+visuallyHideElement('.comments-loader');
