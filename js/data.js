f5260a42271701f37fe9f0f036a2e5fce288e70e
MasterOfMenace <nero_wolf@mail.ru>
2019-10-03 22:34:38 +0300

    Вынес константы с количеством аватарок, количеством комментариев, количеством лайков и количеством фоторгафий в файл rules.js

--- js/data.js
+++ js/data.js
@@ -11,22 +11,22 @@ window.data = (function () {
 
   var MOCK_DESCRIPTIONS_THIRD = ['профессию', 'трубку', 'Икарус', 'покушать', 'плот'];
 
-  var COMMENTS = {
-    MIN: 1,
-    MAX: 3,
-  };
+  // var COMMENTS = {
+  //   MIN: 1,
+  //   MAX: 3,
+  // };
 
-  var AVATARS = {
-    MIN: 1,
-    MAX: 6,
-  };
+  // var AVATARS = {
+  //   MIN: 1,
+  //   MAX: 6,
+  // };
 
-  var LIKES = {
-    MIN: 15,
-    MAX: 200,
-  };
+  // var LIKES = {
+  //   MIN: 15,
+  //   MAX: 200,
+  // };
 
-  var PHOTOS_COUNT = 25;
+  // var PHOTOS_COUNT = 25;
 
   function createArray(number, func) {
     var array = [];
@@ -41,21 +41,21 @@ window.data = (function () {
     var photo = {};
     photo.url = 'photos/' + i + '.jpg';
     photo.description = MOCK_DESCRIPTIONS_FIRST[window.util.getRandomNumber(0, MOCK_DESCRIPTIONS_FIRST.length - 1)] + ' ' + MOCK_DESCRIPTIONS_SECOND[window.util.getRandomNumber(0, MOCK_DESCRIPTIONS_SECOND.length - 1)] + ' ' + MOCK_DESCRIPTIONS_THIRD[window.util.getRandomNumber(0, MOCK_DESCRIPTIONS_THIRD.length - 1)];
-    photo.likes = window.util.getRandomNumber(LIKES.MIN, LIKES.MAX);
-    photo.comments = createArray(window.util.getRandomNumber(COMMENTS.MIN, COMMENTS.MAX), createComment);
+    photo.likes = window.util.getRandomNumber(window.rules.LIKES.MIN, window.rules.LIKES.MAX);
+    photo.comments = createArray(window.util.getRandomNumber(window.rules.COMMENTS.MIN, window.rules.COMMENTS.MAX), createComment);
     return photo;
   }
 
   // создает комментарий
   function createComment() {
     var comment = {};
-    comment.avatar = 'img/avatar-' + window.util.getRandomNumber(AVATARS.MIN, AVATARS.MAX) + '.svg';
+    comment.avatar = 'img/avatar-' + window.util.getRandomNumber(window.rules.AVATARS.MIN, window.rules.AVATARS.MAX) + '.svg';
     comment.message = MOCK_COMMENTS[window.util.getRandomNumber(0, MOCK_COMMENTS.length - 1)];
     comment.name = MOCK_NAMES[window.util.getRandomNumber(0, MOCK_NAMES.length - 1)];
     return comment;
   }
 
-  var photos = createArray(PHOTOS_COUNT, createPhoto);
+  var photos = createArray(window.rules.PHOTOS_COUNT, createPhoto);
 
   return {
     photos: photos,
