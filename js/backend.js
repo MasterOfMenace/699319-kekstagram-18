05b55f9eba3ed7c83289e1ddb4c211bb40f15dd9
MasterOfMenace <nero_wolf@mail.ru>
2019-10-05 21:53:56 +0300

    Вынес статус ответа сервера в константу

--- js/backend.js
+++ js/backend.js
@@ -1,6 +1,8 @@
 'use strict';
 
 window.backend = (function () {
+  var XHR_SUCCESS_STATUS = 200;
+
   function load(onSucess, onError) {
     var URL = 'https://js.dump.academy/kekstagram/data';
 
@@ -10,7 +12,7 @@ window.backend = (function () {
     xhr.open('GET', URL);
 
     xhr.addEventListener('load', function () {
-      if (xhr.status === 200) {
+      if (xhr.status === XHR_SUCCESS_STATUS) {
         onSucess(xhr.response);
       } else {
         onError('Статус ответа ' + xhr.status + xhr.statusText);
