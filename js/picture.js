97a2721de00342d10a63a652f916323a696b25b6
MasterOfMenace <nero_wolf@mail.ru>
2019-10-13 14:12:42 +0300

    Удалил лишний закомментированный код

--- js/picture.js
+++ js/picture.js
@@ -38,67 +38,6 @@ window.picture = (function () {
     main.appendChild(errorNode);
   }
 
-  // function shuffleArray(array, number) {
-  //   var inputArray = array.slice();
-  //   var outputArray = [];
-  //   for (var i = 0; i < number; i++) {
-  //     var index = Math.floor(Math.random() * inputArray.length);
-  //     outputArray.push(inputArray[index]);
-  //     inputArray.splice(index, 1);
-  //   }
-  //   return outputArray;
-  // }
-
-  // var filters = document.querySelector('.img-filters');
-  // var filterButtons = filters.querySelectorAll('button');
-  // var popular = filters.querySelector('#filter-popular');
-  // var random = filters.querySelector('#filter-random');
-  // var discussed = filters.querySelector('#filter-discussed');
-
-  // function clearPicturesContainer() {
-  //   var pictures = photosContainer.querySelectorAll('.picture');
-  //   pictures.forEach(function (elem) {
-  //     elem.remove();
-  //   });
-  // }
-
-  // function toggleButtonActiveClass() {
-  //   Array.prototype.filter.call(filterButtons, function (button) {
-  //     if (button.classList.contains('img-filters__button--active')) {
-  //       button.classList.remove('img-filters__button--active');
-  //     }
-  //   });
-  // }
-
-  // function randomButtonClickHandler(evt) {
-  //   clearPicturesContainer();
-  //   var shuffled = shuffleArray(window.photos.slice(), 10);
-  //   renderPhotos(shuffled);
-  //   toggleButtonActiveClass();
-  //   evt.target.classList.add('img-filters__button--active');
-  // }
-
-  // function popularButtonClickHandler(evt) {
-  //   clearPicturesContainer();
-  //   renderPhotos(window.photos);
-  //   toggleButtonActiveClass();
-  //   evt.target.classList.add('img-filters__button--active');
-  // }
-
-  // function discussedButtonClickHandler(evt) {
-  //   clearPicturesContainer();
-  //   var sorted = window.photos.slice().sort(function (a, b) {
-  //     return b.comments.length - a.comments.length;
-  //   });
-  //   renderPhotos(sorted);
-  //   toggleButtonActiveClass();
-  //   evt.target.classList.add('img-filters__button--active');
-  // }
-
-  // random.addEventListener('click', randomButtonClickHandler);
-  // popular.addEventListener('click', popularButtonClickHandler);
-  // discussed.addEventListener('click', discussedButtonClickHandler);
-
   return {
     onSuccess: onSuccess,
     renderError: renderError,
